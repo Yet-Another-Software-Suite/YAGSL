eecaba9a61f3f62f4105284771368097698d5d6d
twisterjafla <twiserjafla@gmail.com>
2026-01-11 23:48:47 -0700

    made changes

---
+++ yagsl/java/swervelib/simulation/ironmaple/simulation/seasonspecific/rebuilt2026/RebuiltFuelOnFly.java
@@ -0,0 +1,62 @@
+package swervelib.simulation.ironmaple.simulation.seasonspecific.rebuilt2026;
+
+import static edu.wpi.first.units.Units.Inches;
+import static edu.wpi.first.units.Units.Meters;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.units.measure.LinearVelocity;
+import swervelib.simulation.ironmaple.simulation.gamepieces.GamePieceProjectile;
+import swervelib.simulation.ironmaple.simulation.seasonspecific.reefscape2025.ReefscapeAlgaeOnField;
+
+/**
+ *
+ *
+ * <h1>Represents an FUEL launched into the air.</h1>
+ *
+ * <p>This class models a {@link RebuiltFuelOnField} launched into the air.
+ *
+ * <p>The simulation will determine if the FUEL hits its target—the HUB.
+ *
+ * <p>The user can specify a callback using {@link #setHitNetCallBack(Runnable)}, which will be triggered when the FUEL
+ * hits the HUB.
+ */
+public class RebuiltFuelOnFly extends GamePieceProjectile {
+    private static Runnable hitNetCallBack = () -> {};
+
+    /**
+     *
+     *
+     * <h2>Specifies a callback for when any fuel launched into the air hits the hub.</h2>
+     *
+     * @param callBack a {@link Runnable} to be invoked when an fuel hits the hub
+     */
+    public static void setHitNetCallBack(Runnable callBack) {
+        hitNetCallBack = callBack;
+    }
+
+    public RebuiltFuelOnFly(
+            Translation2d robotPosition,
+            Translation2d shooterPositionOnRobot,
+            ChassisSpeeds chassisSpeeds,
+            Rotation2d shooterFacing,
+            Distance initialHeight,
+            LinearVelocity launchingSpeed,
+            Angle shooterAngle) {
+        super(
+                RebuiltFuelOnField.REBUILT_FUEL_INFO,
+                robotPosition,
+                shooterPositionOnRobot,
+                chassisSpeeds,
+                shooterFacing,
+                initialHeight,
+                launchingSpeed,
+                shooterAngle);
+
+        super.withTouchGroundHeight(Inches.of(3).in(Meters));
+        super.enableBecomesGamePieceOnFieldAfterTouchGround();
+    }
+}
