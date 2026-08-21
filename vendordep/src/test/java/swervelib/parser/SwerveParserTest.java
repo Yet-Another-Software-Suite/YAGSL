4a84d3351bfcfcd30d35dca05c969348c9d843ec
DanPeled <danpeled31@gmail.com>
2026-08-21 17:02:16 +0300

    fix format

--- vendordep/src/test/java/swervelib/parser/SwerveParserTest.java
+++ vendordep/src/test/java/swervelib/parser/SwerveParserTest.java
@@ -112,41 +112,42 @@ class SwerveParserTest {
   void testParsesSwervedriveNotNull() throws IOException {
     File dir = createTestConfigDir();
     SwerveParser.parse(dir);
+    ;
     assertNotNull(SwerveParser.swerveDriveJson);
   }
 
   @Test
   void testParsesSwervedriveGyroType() throws IOException {
     File dir = createTestConfigDir();
-    SwerveParser.parse(dir)
+    SwerveParser.parse(dir);
     assertEquals("pigeon2_can", SwerveParser.swerveDriveJson.gyro.type);
   }
 
   @Test
   void testParsesSwervedriveGyroId() throws IOException {
     File dir = createTestConfigDir();
-    SwerveParser.parse(dir)
+    SwerveParser.parse(dir);
     assertEquals(0, SwerveParser.swerveDriveJson.gyro.id);
   }
 
   @Test
   void testParsesSwervedriveGyroAxis() throws IOException {
     File dir = createTestConfigDir();
-    SwerveParser.parse(dir)
+    SwerveParser.parse(dir);
     assertEquals("yaw", SwerveParser.swerveDriveJson.gyroAxis);
   }
 
   @Test
   void testParsesSwervedriveGyroInvert() throws IOException {
     File dir = createTestConfigDir();
-    SwerveParser.parse(dir)
+    SwerveParser.parse(dir);
     assertFalse(SwerveParser.swerveDriveJson.gyroInvert);
   }
 
   @Test
   void testParsesSwervedriveModulesLength() throws IOException {
     File dir = createTestConfigDir();
-    SwerveParser.parse(dir)
+    SwerveParser.parse(dir);
     assertEquals(4, SwerveParser.swerveDriveJson.modules.length);
   }
 
@@ -155,42 +156,42 @@ class SwerveParserTest {
   @Test
   void testParsesPidfPropertiesNotNull() throws IOException {
     File dir = createTestConfigDir();
-    SwerveParser.parse(dir)
+    SwerveParser.parse(dir);
     assertNotNull(SwerveParser.pidfPropertiesJson);
   }
 
   @Test
   void testParsesPidfDriveP() throws IOException {
     File dir = createTestConfigDir();
-    SwerveParser.parse(dir)
+    SwerveParser.parse(dir);
     assertEquals(0.5, SwerveParser.pidfPropertiesJson.drive.p, 0.001);
   }
 
   @Test
   void testParsesPidfDriveI() throws IOException {
     File dir = createTestConfigDir();
-    SwerveParser.parse(dir)
+    SwerveParser.parse(dir);
     assertEquals(0.0, SwerveParser.pidfPropertiesJson.drive.i, 0.001);
   }
 
   @Test
   void testParsesPidfDriveD() throws IOException {
     File dir = createTestConfigDir();
-    SwerveParser.parse(dir)
+    SwerveParser.parse(dir);
     assertEquals(0.01, SwerveParser.pidfPropertiesJson.drive.d, 0.001);
   }
 
   @Test
   void testParsesPidfAngleP() throws IOException {
     File dir = createTestConfigDir();
-    SwerveParser.parse(dir)
+    SwerveParser.parse(dir);
     assertEquals(2.0, SwerveParser.pidfPropertiesJson.angle.p, 0.001);
   }
 
   @Test
   void testParsesPidfAngleD() throws IOException {
     File dir = createTestConfigDir();
-    SwerveParser.parse(dir)
+    SwerveParser.parse(dir);
     assertEquals(0.1, SwerveParser.pidfPropertiesJson.angle.d, 0.001);
   }
 
@@ -199,42 +200,42 @@ class SwerveParserTest {
   @Test
   void testParsesPhysicalPropertiesNotNull() throws IOException {
     File dir = createTestConfigDir();
-    SwerveParser.parse(dir)
+    SwerveParser.parse(dir);
     assertNotNull(SwerveParser.physicalPropertiesJson);
   }
 
   @Test
   void testParsesPhysicalDriveGearRatio() throws IOException {
     File dir = createTestConfigDir();
-    SwerveParser.parse(dir)
+    SwerveParser.parse(dir);
     assertEquals(6.75, SwerveParser.physicalPropertiesJson.gearing.drive.gearRatio, 0.001);
   }
 
   @Test
   void testParsesPhysicalDriveDiameter() throws IOException {
     File dir = createTestConfigDir();
-    SwerveParser.parse(dir)
+    SwerveParser.parse(dir);
     assertEquals(4.0, SwerveParser.physicalPropertiesJson.gearing.drive.diameter, 0.001);
   }
 
   @Test
   void testParsesPhysicalAngleGearRatio() throws IOException {
     File dir = createTestConfigDir();
-    SwerveParser.parse(dir)
+    SwerveParser.parse(dir);
     assertEquals(12.8, SwerveParser.physicalPropertiesJson.gearing.angle.gearRatio, 0.001);
   }
 
   @Test
   void testParsesPhysicalStatorCurrentLimitDrive() throws IOException {
     File dir = createTestConfigDir();
-    SwerveParser.parse(dir)
+    SwerveParser.parse(dir);
     assertEquals(40, SwerveParser.physicalPropertiesJson.statorCurrentLimit.drive);
   }
 
   @Test
   void testParsesPhysicalStatorCurrentLimitAngle() throws IOException {
     File dir = createTestConfigDir();
-    SwerveParser.parse(dir)
+    SwerveParser.parse(dir);
     assertEquals(20, SwerveParser.physicalPropertiesJson.statorCurrentLimit.angle);
   }
 
@@ -243,63 +244,63 @@ class SwerveParserTest {
   @Test
   void testParsesAllModulesNotNull() throws IOException {
     File dir = createTestConfigDir();
-    SwerveParser.parse(dir)
+    SwerveParser.parse(dir);
     assertNotNull(SwerveParser.moduleJsons);
   }
 
   @Test
   void testParsesAllModulesLength() throws IOException {
     File dir = createTestConfigDir();
-    SwerveParser.parse(dir)
+    SwerveParser.parse(dir);
     assertEquals(4, SwerveParser.moduleJsons.length);
   }
 
   @Test
   void testParsesFlModuleDriveType() throws IOException {
     File dir = createTestConfigDir();
-    SwerveParser.parse(dir)
+    SwerveParser.parse(dir);
     assertEquals("sparkmax_neo", SwerveParser.moduleJsons[0].drive.type);
   }
 
   @Test
   void testParsesFlModuleDriveId() throws IOException {
     File dir = createTestConfigDir();
-    SwerveParser.parse(dir)
+    SwerveParser.parse(dir);
     assertEquals(1, SwerveParser.moduleJsons[0].drive.id);
   }
 
   @Test
   void testParsesFlModuleLocationFront() throws IOException {
     File dir = createTestConfigDir();
-    SwerveParser.parse(dir)
+    SwerveParser.parse(dir);
     assertEquals(12.0, SwerveParser.moduleJsons[0].location.front, 0.001);
   }
 
   @Test
   void testParsesFlModuleLocationLeft() throws IOException {
     File dir = createTestConfigDir();
-    SwerveParser.parse(dir)
+    SwerveParser.parse(dir);
     assertEquals(12.0, SwerveParser.moduleJsons[0].location.left, 0.001);
   }
 
   @Test
   void testParsesBrModuleDriveId() throws IOException {
     File dir = createTestConfigDir();
-    SwerveParser.parse(dir)
+    SwerveParser.parse(dir);
     assertEquals(7, SwerveParser.moduleJsons[3].drive.id);
   }
 
   @Test
   void testParsesBrModuleLocationFront() throws IOException {
     File dir = createTestConfigDir();
-    SwerveParser.parse(dir)
+    SwerveParser.parse(dir);
     assertEquals(-12.0, SwerveParser.moduleJsons[3].location.front, 0.001);
   }
 
   @Test
   void testParsesBrModuleLocationLeft() throws IOException {
     File dir = createTestConfigDir();
-    SwerveParser.parse(dir)
+    SwerveParser.parse(dir);
     assertEquals(-12.0, SwerveParser.moduleJsons[3].location.left, 0.001);
   }
 
@@ -308,14 +309,14 @@ class SwerveParserTest {
   @Test
   void testModuleDefaultAbsoluteEncoderInverted() throws IOException {
     File dir = createTestConfigDir();
-    SwerveParser.parse(dir)
+    SwerveParser.parse(dir);
     assertFalse(SwerveParser.moduleJsons[0].absoluteEncoderInverted);
   }
 
   @Test
   void testModuleDefaultAbsoluteEncoderGearRatio() throws IOException {
     File dir = createTestConfigDir();
-    SwerveParser.parse(dir)
+    SwerveParser.parse(dir);
     assertEquals(1.0, SwerveParser.moduleJsons[0].absoluteEncoderGearRatio, 0.001);
   }
 
@@ -351,21 +352,21 @@ class SwerveParserTest {
   @Test
   void testGyroAxisYawParsed() throws IOException {
     File dir = createTestConfigDirWithGyroAxis("yaw");
-    SwerveParser.parse(dir)
+    SwerveParser.parse(dir);
     assertEquals("yaw", SwerveParser.swerveDriveJson.gyroAxis);
   }
 
   @Test
   void testGyroAxisPitchParsed() throws IOException {
     File dir = createTestConfigDirWithGyroAxis("pitch");
-    SwerveParser.parse(dir)
+    SwerveParser.parse(dir);
     assertEquals("pitch", SwerveParser.swerveDriveJson.gyroAxis);
   }
 
   @Test
   void testGyroAxisRollParsed() throws IOException {
     File dir = createTestConfigDirWithGyroAxis("roll");
-    SwerveParser.parse(dir)
+    SwerveParser.parse(dir);
     assertEquals("roll", SwerveParser.swerveDriveJson.gyroAxis);
   }
 
